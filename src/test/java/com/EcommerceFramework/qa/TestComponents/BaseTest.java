bb58823a221f26dda74687e7aea81bcdf7f31829
Dinesh A <dinesh.014@gmail.com>
2024-02-13 10:44:49 +0530

    adding headless options

--- src/test/java/com/EcommerceFramework/qa/TestComponents/BaseTest.java
+++ src/test/java/com/EcommerceFramework/qa/TestComponents/BaseTest.java
@@ -12,10 +12,12 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -44,8 +46,13 @@ public class BaseTest {
 			browserName = System.getProperty("browser")!=null
 					?System.getProperty("browser"):prop.getProperty("browser");
 			
-			if (browserName.equals("chrome")) {
-				driver = new ChromeDriver();
+			if (browserName.contains("chrome")) {
+				ChromeOptions options = new ChromeOptions();
+				if (browserName.contains("headless")) {
+					options.addArguments("headless");
+				}
+				driver = new ChromeDriver(options);
+				driver.manage().window().setSize(new Dimension(1440, 900));
 			} else if (browserName.equals("edge")) {
 				driver = new EdgeDriver();
 			} else if (browserName.equals("firefox")) {
